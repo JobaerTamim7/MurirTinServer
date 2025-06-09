a41c81cd9cb5212d249689f6673a3b6a3dca3d50
Tamim <jobaertamim7@gmail.com>
2025-06-09 14:07:18 +0600

    feat(router): add login without jwt

--- app/api/Auth/router.py
+++ app/api/Auth/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends
-from schemas.UserSchema import CreateUserRequest
+from schemas.UserSchema import CreateUserRequest,LoginUserRequest
 from crud.User.create_user import create_user
+from services.Auth.login_validation import login_validation
 from sqlalchemy.orm import Session
+from models.User import User
 from database.database import get_session
 
 auth_router = APIRouter(prefix="/auth",tags=["auth"])
@@ -9,3 +11,7 @@ auth_router = APIRouter(prefix="/auth",tags=["auth"])
 @auth_router.post("/register")
 async def register(user: CreateUserRequest, db_session: Session = Depends(get_session)):
     return create_user(user,db_session)
+
+@auth_router.post("/login")
+async def login(req_user: LoginUserRequest, db_session: Session = Depends(get_session)):
+    return login_validation(req_user,db_session)
--- app/services/Auth/login_validation.py
+++ app/services/Auth/login_validation.py
@@ -5,17 +5,15 @@ from models.User import User
 from sqlalchemy.orm import Session
 from fastapi import HTTPException,status
 from fastapi.responses import JSONResponse
-from exceptions import UserNotFoundError, PasswordMismatchError
 
-def login_validation(req_user: LoginUserRequest, db_session: Session) -> JSONResponse | None:
+def login_validation(req_user: LoginUserRequest, db_session: Session) -> JSONResponse:
     try:
         user: User = get_user_by_mail(req_user.email, db_session)
         is_pass_verified: bool = verify_pass(req_user.password.get_secret_value(),user.hashed_password)
 
         if is_pass_verified == False:
-            raise PasswordMismatchError()
+            raise HTTPException(status_code=401,detail="Wrong password.")
         
-    
         user_info : UserInfo = UserInfo(
                 email=user.email,
                 user_name=user.user_name,
@@ -30,13 +28,7 @@ def login_validation(req_user: LoginUserRequest, db_session: Session) -> JSONRes
                 "user": user_info.model_dump()
             }
         )
-    except Exception as error:
-        handleError(error)
-
-def handleError(error: Exception):
-    if isinstance(error, UserNotFoundError):
-        raise HTTPException(status_code=404, detail="User is not registered.")
-    elif isinstance(error, PasswordMismatchError):
-        raise HTTPException(status_code=401, detail="Password is incorrect.")
-    else:
-        raise HTTPException(status_code=500, detail=f"Internal Server Error. {str(error)}")
+    except HTTPException:
+        raise
+    except Exception:
+        raise HTTPException(status_code=500, detail="Internal server error") 
